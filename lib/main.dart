623f18fc322df239863000339905b81cc2e45a93
Jayasoorya K Deepak <jayasooryakdeepak@gmail.com>
2023-05-28 17:57:45 +0530

    Bottom Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_app/core/colors/colors.dart';
+import 'package:netflix_app/presentation/main_page/widgets/screen_main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +15,18 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blue,
+        ).copyWith(
+          background: Colors.black,
+        ),
+        scaffoldBackgroundColor: backgroundColor,
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
+        ),
       ),
-      home: MyHomePage(),
+      home: ScreenMainPage(),
     );
   }
 }
---
+++ lib/presentation/main_page/bottom_nav.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+ValueNotifier<int> indexChangeNotifier = ValueNotifier(0);
+
+class BottomNavigationWidget extends StatelessWidget {
+  const BottomNavigationWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      valueListenable: indexChangeNotifier,
+      builder: (context, int newIndex, _) {
+        return BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: newIndex,
+          onTap: (index) {
+            indexChangeNotifier.value = index;
+          },
+          backgroundColor: Colors.black,
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.grey,
+          selectedIconTheme: const IconThemeData(color: Colors.white),
+          unselectedIconTheme: const IconThemeData(color: Colors.blueGrey),
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.home,
+                ),
+                label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.collections,
+                ),
+                label: 'New & Hot'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.emoji_emotions_outlined,
+                ),
+                label: 'Fast Laugh'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.search_rounded,
+                ),
+                label: 'Search'),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.download,
+                ),
+                label: 'Downloads'),
+          ],
+        );
+      },
+    );
+  }
+}
